e3b6e829097940e84c9cde5fdcd6f8ca04b9580a
Ivan Trejo <trejoivan258@gmail.com>
2022-09-02 16:02:41 -0700

    completed first workbook hw

---
+++ Workbook/1/Workbook.java
@@ -0,0 +1,8 @@
+public class Workbook {
+  public static void main(String[] args){
+    System.out.println("Printing is Fun!");
+    System.out.println("Java over Python");
+    System.out.println("Money over everything!");
+    
+  }
+}
